5b6a832491524dbb9a9212ff2dc4dbf03ba262b0
danilasimanok <dsimanok@bk.ru>
2016-12-02 19:55:13 +0300

    Игра зациклена до поражения всех игроков, работает Double. Сделать лист имен игроков, Split, новый вид ставочника.

--- src/com/company/Computer.java
+++ src/com/company/Computer.java
@@ -4,8 +4,10 @@ package com.company;
  * Created by falyanguzov on 14.11.2016.
  */
 public class Computer extends Player {
+    String[] names = {"jhbj","jvhbj"};
 
     public Computer(Intellect intellect,String name,Stavochnik stavochnik) {
-        super(intellect, name,stavochnik);
+        super(intellect,name,stavochnik);
+       // this.name ="jjjj";
     }
 }
